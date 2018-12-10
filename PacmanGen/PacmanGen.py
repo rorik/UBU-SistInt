f5bca3e8a44bb37a9231b67bb8cd2a4b5d18086a
Roderick Diaz <about@rorik.me>
2018-12-10 23:34:35 +0100

    Improved PacmanGen evaluation function

--- PacmanGen/PacmanGen.py
+++ PacmanGen/PacmanGen.py
@@ -1,14 +1,11 @@
 import numpy as np
 import random
-
-from deap import algorithms
-from deap import base
-from deap import creator
-from deap import tools
-import deap
+import argparse
+import pickle
+import time
 
 from Game.Gamestate import Gamestate
-from Game.Board import get_default_board, get_default_food
+from Game.Board import BoardState, get_default_board, get_default_food
 from AI.NeuralNetwork import NeuralNetwork
 from AI.Individual import Individual
 
@@ -18,7 +15,8 @@ moves = [[-1, 0], [0, 1], [1, 0], [0, -1]]
 def play(model, verbose=False):
     global moves
     moved = []
-    game = Gamestate(get_default_board(), get_default_food(), max_moves)
+    visited = []
+    game = Gamestate(board, BoardState(food), max_moves)
     while not game.has_ended():
         input_data = game.get_data()
         predict = model.predict(input_data)[0]
@@ -26,6 +24,14 @@ def play(model, verbose=False):
         move = moves[output]
         result = game.move(move)
         moved.append([output, result])
+        if result == -5:  # Wall, neural network can't advance
+            break
+        if result > 0:  # Food, reset visited nodes
+            visited = []
+        else:
+            if any(pos[0] == game.player[0] and pos[1] == game.player[1] for pos in visited):
+                break  # Already was in this state, neural network can't advance
+        visited.append(game.player)
     if verbose:
         print(" ENDED WITH SCORE", game.score, "AFTER", game.moves, "MOVES")
     return game, moved
@@ -33,18 +39,8 @@ def play(model, verbose=False):
 
 def evaluate(individual):
     result, moved = play(individual.model)
-    fitness = 0
-    #fitness = result.score / result.moves * 200
-    for i in range(len(moved)):
-        if False: #i - 2 >= 0:
-            previous = (moved[i - 1][0] + 2) % 4
-            previous2 = moved[i-2][0]
-            if previous == previous2 == moved[i][0]:
-                print(previous, previous2, moved[i][0])
-                fitness += moved[i][1] * 3
-                continue
-        fitness += moved[i][1]
-    return fitness
+    fitness = len([move for move in moved if move[1] > 0])
+    return fitness, moved
 
 
 def breed(individual1, individual2):
@@ -65,24 +61,16 @@ def breed(individual1, individual2):
 
 
 def mutate(individual):
-    # print("Mutating", individual)
-    # for layer in range(len(individual.model.layers)):
-    #     w = individual.model.layers[layer].get_weights()[0]
-    #     w_mask_min = np.invert(np.random.randint(0, int(1 / mutation_severity), size=w.shape).astype(np.bool))
-    #     w_random_min = np.random.rand(*w.shape) * np.min(w)
-    #     w_mask_max = np.invert(np.random.randint(0, int(1 / mutation_severity), size=w.shape).astype(np.bool))
-    #     w_random_max = np.random.rand(*w.shape) * np.max(w)
-    #     w[w_mask_min] = w_random_min[w_mask_min]
-    #     w[w_mask_max] = w_random_max[w_mask_max]
-    #     b = individual.model.layers[layer].get_weights()[1]
-    #     b_mask_min = np.invert(np.random.randint(0, int(1 / mutation_severity), size=b.shape).astype(np.bool))
-    #     b_random_min = np.random.rand(*b.shape) * (np.min(b))
-    #     b_mask_max = np.invert(np.random.randint(0, int(1 / mutation_severity), size=b.shape).astype(np.bool))
-    #     b_random_max = np.random.rand(*b.shape) * (np.max(b))
-    #     b[b_mask_min] = b_random_min[b_mask_min]
-    #     b[b_mask_max] = b_random_max[b_mask_max]
-    #     individual.model.layers[layer].set_weights(np.asarray([w, b]))
-    pass
+    for layer in range(len(individual.model.layers)):
+        w = individual.model.layers[layer].get_weights()[0]
+        w_mask = np.invert(np.random.randint(0, int(1 / mutation_severity), size=w.shape).astype(np.bool))
+        w_random = np.random.uniform(low=-1, high=1, size=w.shape)
+        w[w_mask] = w_random[w_mask]
+        b = individual.model.layers[layer].get_weights()[1]
+        b_mask = np.invert(np.random.randint(0, int(1 / mutation_severity), size=b.shape).astype(np.bool))
+        b_random = np.random.uniform(low=-1, high=1, size=b.shape)
+        b[b_mask] = b_random[b_mask]
+        individual.model.layers[layer].set_weights(np.asarray([w, b]))
 
 
 def select(individuals):
@@ -91,31 +79,27 @@ def select(individuals):
     for individual in individuals:
         result = evaluate(individual)
         evaluation.append([individual, result])
-        evaluation_stat.append(result)
+        evaluation_stat.append(result[0])
 
     # sort individuals by their evaluation
-    evaluation.sort(key=lambda x: x[1], reverse=True)
+    evaluation.sort(key=lambda x: x[1][0], reverse=True)
 
-    # keep the best individuals
+    # Store the best individual
+    best.append([evaluation[0][1][0], evaluation[0][1][1], evaluation[0][0].model.to_json()])
+
+    # Keep the best individuals
     retained = int(population_size * retain)
     selected = [x[0] for x in evaluation[:retained]]
 
-    for x in evaluation:
-        result = play(x[0].model, True)
-        print("--------")
-        print(x[1])
-        print(result[1])
-
-    #result = play(selected[0].model, True)
-    #print(selected[0].model.get_weights())
-    #print(result[0])
-    #print(result[1])
-    #print(evaluation[0][1])
-
-    # keep some of the "bad" individuals
+    # Keep some of the "bad" individuals
     for non_parent in evaluation[retained:]:
         if random.random() <= retain_chance:
             selected.append(non_parent[0])
+
+    # If not enough individuals, add a random one
+    while len(selected) < 2:
+        selected.append(random.choice(evaluation)[0])
+
     return selected, evaluation_stat
 
 
@@ -128,19 +112,47 @@ def populate(individual_class, size=20):
 
 if __name__ == '__main__':
 
-    board = get_default_board()
+    parser = argparse.ArgumentParser(description='Pacman genetic neural network algorithm.')
+    parser.add_argument("-l", "--load", type=str, help="load the population from a file")
+    parser.add_argument("-s", "--save", type=str, help="save the population to a file")
+    parser.add_argument("-b", "--best", type=str, help="save the best individual of each generation to a file")
+    parser.add_argument("-g", "--generations", type=int, help="save the best individual of each generation to a file")
+    args = parser.parse_args()
 
-    generations = 1
-    population_size = 8
-    max_moves = 30
+    board = get_default_board()
+    food = get_default_food()
+
+    if args.generations:
+        generations = args.generations
+    else:
+        generations = 20
+    population_size = 10
+    max_moves = 300
     retain = 0.3
     retain_chance = 0.1
-    mutate_chance = 0.3
+    mutate_chance = 0.2
     mutation_severity = 0.1
 
-    population = populate(Individual, population_size)
-
+    if args.load is not None:
+        print("Loading population from file")
+        population = []
+        try:
+            with open(args.load, "rb") as file:
+                loaded = pickle.load(file)
+                for weights in loaded:
+                    model = NeuralNetwork.get_new_model(board)
+                    model.set_weights(weights)
+                    population.append(Individual(model))
+        except IOError:
+            print("Error reading file")
+            raise
+    else:
+        population = populate(Individual, population_size)
+    best = []
     for i in range(generations):
+        start = time.time()
+
+        # Get the new parents
         parents, evaluation_stat = select(population)
 
         # Short-circuit if not enough parents
@@ -162,11 +174,14 @@ if __name__ == '__main__':
             new_population.append(child)
 
         population = new_population
-        print("Generation %s (avg:%.2f, max:%.2f, min: %.2f)" % (i, sum(evaluation_stat) / len(evaluation_stat),
-                                                                 max(evaluation_stat), min(evaluation_stat)))
+        end = time.time()
+        print("Generation %s (avg:%.2f, max:%.2f, min: %.2f) took %.2fs" %
+              (i, sum(evaluation_stat) / len(evaluation_stat), max(evaluation_stat), min(evaluation_stat), end - start))
+        if i == generations - 1 and args.save is not None:
+            output = [individual.model.get_weights() for individual in population]
+            with open(args.save, "wb") as file:
+                pickle.dump(output, file)
 
-
-
-
-    #nn_model = NeuralNetwork.get_new_model(board)
-    #play(nn_model)
+    if args.best is not None:
+        with open(args.best, "a+") as file:
+            file.writelines(["%d %% %s %% %s\n" % (record[0], record[1], record[2]) for record in best])
